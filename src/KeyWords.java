662ff9c629cb0ee441fa557f75d1b4dbb6aa7d80
Sepanta <sepanta1380@gmail.com>
2021-04-15 15:19:19 +0430

    Regex

---
+++ src/KeyWords.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class KeyWords {
+    public static String[] keywords = {"void", "int", "double", "bool", "string", "class", "null", "this", "for", "while", "if", "else", "return", "break", "continue",
+            "new", "NewArray", "getArrVal", "Print", "ReadInteger", "ReadLine", "dtoi", "itod", "btoi", "itob", "private","public", "true", "false"};
+    public static ArrayList<String> keywordList = new ArrayList<>();
+    public static void setKeywordList(){
+        keywordList.addAll(Arrays.asList(keywords));
+    }
+
+}
--- src/RegexHandler.java
+++ src/RegexHandler.java
@@ -1,4 +1,10 @@
 public class RegexHandler {
-
-    public static final String decimal_number_regex = "";
+    public static final String identifier_regex = "^[a-zA-Z][\\w_]*";
+    public static final String decimal_integer_regex = "^\\d+";
+    public static final String hex_integer_regex = "^0[Xx][\\da-fA-F]+";
+    public static final String double_regex = "^\\d+\\.\\d*([Ee][+-]?\\d+)?";
+    public static final String string_regex = "^\\\"[^\\\"\\n]*\\\"";
+    public static final String boolean_regex = "^true|^false";
+    public static final String operator_regex = "^<=|^>=|^\\+=|^-=|^\\*=|^\\/=|^==|^!=|^&&|^\\|\\||^\\+|^-|^\\*|^\\/|^%|^<|^>|^=|^!|^;|^,|^\\.|^\\[|^\\]|^\\(|^\\)";
+    public static final String comment_regex= "^\\/\\/[^\\n]*\\n|^\\/\\*.*\\*\\/";
 }
