83ead03278edffa7416e687573f878536166d57b
morabba <hey>
2021-07-22 16:06:36 +0430

    minor debug

--- tests/code_generator/t001-io1.d
+++ tests/code_generator/t001-io1.d
@@ -2,14 +2,16 @@
 int a(int x) {
     Print(1);
     Print(x);
-    b(x);
+    b(x, 14);
 }
 
 int main() {
    a(13);
 }
 
-void b(int x) {
+void b(int x, int y) {
     Print(2);
     Print(x);
+    Print(3);
+    Print(y);
 }
